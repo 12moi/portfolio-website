16882593ae9ec58f595971257d1390cd7741b190
12moi <moi.shadrack@student.moringaschool.com>
2022-06-14 08:22:09 -0700

    add project folders

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import header from './components/header/Header.jsx'
+import nav from './components/nav/Nav.jsx'
+
+const App = () => {
+  return (
+    <div>
+        <header/>
+        <nav/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './experience.css'
+function experience() {
+  return (
+    <div>experience</div>
+  )
+}
+
+export default experience
