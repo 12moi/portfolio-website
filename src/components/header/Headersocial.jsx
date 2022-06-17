14b5a52a4088ea451025c941cd5782f2da4e0e88
12moi <moi.shadrack@student.moringaschool.com>
2022-06-17 05:21:52 -0700

    style the header

--- src/components/header/Headersocial.jsx
+++ src/components/header/Headersocial.jsx
@@ -5,7 +5,7 @@ import {FaGithub}  from 'react-icons/fa'
 export class Headersocial extends Component {
   render() {
     return (
-      <div className='header_social'>
+      <div className='header__social'>
         <a href='https://linkedin.com' target="_blank"><BsLinkedin/></a>
         <a href='https://github.com' target="_blank"><FaGithub/></a>
       </div>
