56b371b59592ea06e530052bcdc8b619b6414146
12moi <moi.shadrack@student.moringaschool.com>
2022-06-17 05:40:05 -0700

    add Nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {FaHome} from 'react-icons/fa'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageDetail} from 'react-icons/bi'
 function Nav() {
   return (
-    <div className='Nav'>This is a Nav</div>
+   
+    <nav>
+     <a href='#'><FaHome/></a>
+     <a href='#about'><AiOutlineUser/></a>
+     <a href='#experience'><BiBook/></a>
+     <a href='#services'><RiServiceLine/></a>
+     <a href='#contact'><BiMessageDetail/></a>
+    </nav>
   )
 }
 
