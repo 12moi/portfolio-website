001ad0d60d5456553d8e914e0904b4d022e695ad
12moi <moi.shadrack@student.moringaschool.com>
2022-06-17 03:54:31 -0700

    add assets

--- src/App.js
+++ src/App.js
@@ -4,22 +4,41 @@ import About from './components/About/About'
 import Experience from './components/Experience/Experience'
 import Header  from './components/Header/Header'
 import Contact from './components/Contact/Contact'
-import Portfolio from './components/Portfolio/Portfolio'
+ import Portfolio from './components/Portfolio/Portfolio'
 import Services from './components/Services/Services'
 import Footer from './components/Footer/Footer'
-function App() {
-  return (
-    <div>
-    <Header/>
-    <Nav/>
-    <About/>
-    <Experience/>
-    <Portfolio/>
-    <Services/>
-    <Contact/>
+import { Component } from 'react';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+     <Nav/>
+     <About/>
+     <Experience/>
+     <Portfolio/>
+     <Services/>
+     <Contact/>
     <Footer/>
-    </div>
-  )
+      </div>
+    );
+  }
 }
+ 
+export default App;
+// function App() {
+//   return (
+//     <div>
+//     <Header/>
+//     <Nav/>
+//     <About/>
+//     <Experience/>
+//     <Portfolio/>
+//     <Services/>
+//     <Contact/>
+//     <Footer/>
+//     </div>
+//   )
+// }
 
-export default App
+// export default App
--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-// import CV from '../../assets/cv.pdf'
+import CV from '../../assets/cv.pdf'
 function CTA() {
   return (
     <div className='cta'>
-        {/* <a href={CV} download className='btn'>Download CV</a>  */}
+        <a href={CV} download className='btn'>Download CV</a> 
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
