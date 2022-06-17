1358694d2d55232d86a08c4f0f9f625667ba769f
12moi <moi.shadrack@student.moringaschool.com>
2022-06-17 07:01:22 -0700

    style the floating navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,15 +5,17 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 import {BiMessageDetail} from 'react-icons/bi'
+import {useState} from 'react'
 function Nav() {
+  const [activeNav, setActiveNav]=useState('#');
   return (
    
     <nav>
-     <a href='#'><FaHome/></a>
-     <a href='#about'><AiOutlineUser/></a>
-     <a href='#experience'><BiBook/></a>
-     <a href='#services'><RiServiceLine/></a>
-     <a href='#contact'><BiMessageDetail/></a>
+     <a href='#' onClick={() => setActiveNav('#')} className={activeNav=== '#' ? 'active' : ''}><FaHome/></a>
+     <a href='#about' onClick={() => setActiveNav('#about')}  className={activeNav=== '#about' ? 'active' : ''}><AiOutlineUser/></a>
+     <a href='#experience' onClick={() => setActiveNav('#experience')}  className={activeNav=== '#experience' ? 'active' : ''}><BiBook/></a>
+     <a href='#services' onClick={() => setActiveNav('#services')}  className={activeNav=== '#services' ? 'active' : ''}><RiServiceLine/></a>
+     <a href='#contact' onClick={() => setActiveNav('#contact')}  className={activeNav=== '#contact' ? 'active' : ''}><BiMessageDetail/></a>
     </nav>
   )
 }
