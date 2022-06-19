df6df7878bc083eb68cbfe5d6d8d2ebebe10c14b
12moi <moi.shadrack@student.moringaschool.com>
2022-06-19 08:10:16 -0700

    add porfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './portfolio.css'
-
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
 function Portfolio() {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Projects</h5>
+      <h2>Portfolio</h2>
+      <div className='container portfolio__container'>
+       <article className='portfolio__item'>
+        <div className='portfolio__item-image'>
+         <img src={IMG1} alt='' />
+        </div>
+        <h3>This is a portfolio item title</h3>
+        <a href='' className='btn' target='_blank'> Github</a>
+        <a href='' className='btn btn-primary' target='_blank'>Live link</a>
+       </article>
+      </div>
+    </section>
   )
 }
 
